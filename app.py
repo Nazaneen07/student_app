e38eb36e353b2a28bf5ff4477369ac783a154da3
Nazaneen07 <Baguaein@gmail.com>
2026-05-12 17:22:00 +0000

    Draft 1 - routing checkpoint

---
+++ app.py
@@ -0,0 +1,117 @@
+"""
+Nazaneen Baguaei,
+May 14, 2026
+Final Project: Student Learning Management App
+"""
+import sqlite3
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+
+app = Flask(__name__)
+app.secret_key = "dev_secret_key"
+
+# ------------------
+# DATABASE CONNECTION
+# ------------------
+def get_db():
+    conn = sqlite3.connect("flask_db.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+# ------------------
+# LOADING PAGE
+# ------------------
+@app.route('/')
+def home():
+    return redirect(url_for('login'))
+
+# ------------------
+# LOGIN ROUTING
+# ------------------
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        conn = get_db()
+        cursor = conn.cursor()
+        cursor.execute(""" SELECT * FROM users WHERE email = ? AND password = ? """, (email, password))
+        user = cursor.fetchone()
+        conn.close()
+        if user:
+            session['username'] = user['username']
+            return redirect(url_for('dashboard'))
+        else:
+            flash("Invalid email or password")
+    return render_template("login.html")
+
+# ------------------
+# SIGNUP ROUTING
+# ------------------
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        conn = get_db()
+        cursor = conn.cursor()
+        try:
+            cursor.execute(""" INSERT INTO users (username, email, password) VALUES (?,?,?) """, (username, email, password))
+            conn.commit()
+            flash("Account created successfully!")
+            return redirect(url_for('login'))
+        except sqlite3.IntegrityError:
+            flash('Email already exists!')
+        finally:
+            conn.close()
+    return render_template('signup.html')
+
+# ------------------
+# DASHBOARD ROUTING
+# ------------------
+@app.route('/dashboard')
+def dashboard():
+    if 'username' in session:
+        return render_template('dashboard.html', username=session['username'])
+    return redirect(url_for('login'))
+
+# ------------------
+# TO-DO LIST ROUTING
+# ------------------
+@app.route('/todo')
+def todo():
+    if 'username' not in session:
+        return redirect(url_for('login'))
+    return render_template('todo.html', username=session['username'])
+
+# ------------------
+# BLOG ROUTING
+# ------------------
+@app.route('/blog')
+def blog():
+    if 'username' not in session:
+        return redirect(url_for('login'))
+    return render_template('blog.html', username=session['username'])
+
+# ------------------
+# IMAGE UPLOAD ROUTING
+# ------------------
+@app.route('/upload')
+def upload():
+    if 'username' not in session:
+        return redirect(url_for('login'))
+    return render_template('upload.html', username=session['username'])
+
+# ------------------
+# LOGOUT ROUTING
+# ------------------
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
+
+# ------------------
+# RUN APP
+# ------------------
+if __name__ == '__main__':
+    app.run(debug=True)
